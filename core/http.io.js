c7cef6020dcbdee2f5ce4f31a96139873d512e0e
lvjian <lvjian@dayang.com.cn>
2012-12-06 16:27:46 +0800

    对socket.io进行基本封装， 提供默认配置
    exports.start(httpServer), 用来开启socket.io监听，返回socket.io 中的 io 对象

---
+++ core/http.io.js
@@ -0,0 +1,16 @@
+var socket_io = require('socket.io');
+
+exports.listen = function(httpServer) {
+	var io = socket_io.listen(httpServer);
+
+	io.configure(function() {
+		io.set('transports', ['websocket', 'flashsocket', 'xhr-polling']);
+	});
+
+	io.configure('development', function() {
+		io.set('transport', ['websocket', 'xhr-polling']);
+		io.enable('log');
+	});
+	
+	return io;
+};
